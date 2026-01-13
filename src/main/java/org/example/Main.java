152c5bf9d0d512c11d1203ef2bc5e46b1d79b89d
miguelnkono <nkonondememiguel7@gmail.com>
2026-01-13 20:19:47 +0100

    fix a bug on the way command line arguments are handle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,23 +7,21 @@ public class Main {
     private static boolean __had_error__ = false;
 
     public static void main(String[] args) {
-        if (args.length > 1) {
+        if (args.length != 1) {
             System.err.println("Synthax: alglang <scrip.al>");
             System.exit(0);
         }
-        if (args.length == 1) {
-            if (!args[0].endsWith(".al")) {
-                System.err.println("Wrong script file!");
-                System.exit(64);
-            }
-            if (!Files.exists(Path.of(args[0]))) {
-                System.err.printf("File [%s] does not exist!\n", args[0]);
-                System.exit(64);
-            }
-
-            // run the program
-            System.out.println("cool");
+        if (!args[0].endsWith(".al")) {
+            System.err.println("Wrong script file!");
+            System.exit(64);
         }
+        if (!Files.exists(Path.of(args[0]))) {
+            System.err.printf("File [%s] does not exist!\n", args[0]);
+            System.exit(64);
+        }
+
+        // run the program
+        System.out.println("cool");
     }
 
 
